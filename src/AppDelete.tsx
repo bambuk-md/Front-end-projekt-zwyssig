ff9cfea0e8445ff2e63eb97986b214b2a9670f15
luczwyssig <77100623+thefaceslayer12@users.noreply.github.com>
2023-02-02 11:11:49 +0100

    Delete erfolgreich gemacht, werde nun mit PUT weiterarbeiten.

--- src/AppDelete.tsx
+++ src/AppDelete.tsx
@@ -1,9 +1,4 @@
-
-
-import './App.css';
-
 import React, { useState, useEffect } from "react";
-import { getValue } from '@testing-library/user-event/dist/utils';
 
 interface Task {
   id: number;
@@ -13,32 +8,25 @@ interface Task {
 
 const Tasks = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [error, setError] = useState<Error | null>(null); //inspiration von altem React Projekt + https://jasonwatmore.com/post/2020/01/27/react-fetch-http-get-request-examples
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     fetch("http://127.0.0.1:3000/tasks", {
-            method: "GET",
-            headers: {"Content-Type": "application/json" }
-            
-        })
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    })
       .then(res => res.json())
       .then(data => setTasks(data))
       .catch(error => setError(error));
   }, []);
-  const Deleter = (task : Task) => {
-    
-  const [error, setError] = useState<Error | null>(null); 
 
-  useEffect(() => {
-    fetch(`http://127.0.0.1:3000/tasks/${task}`, {
-            method: "DELETE",
-            
-        })
+  const handleDelete = (taskId: number) => {
+    fetch(`http://127.0.0.1:3000/task/${taskId}`, {
+      method: "DELETE"
+    })
       
       .catch(error => setError(error));
-  }, []);
-
-  }
+  };
 
   if (error) {
     return <div>An error occurred: {error.message}</div>;
@@ -47,13 +35,13 @@ const Tasks = () => {
   return (
     <ul>
       {tasks.map(task => (
-        <li key={task.id}>{task.title}
-        <button onClick={Deleter(getValue)}>delete Task</button></li>
+        <li key={task.id}>
+          {task.title}
+          <button onClick={() => handleDelete(task.id)}>delete Task</button>
+        </li>
       ))}
     </ul>
   );
 };
 
 export default Tasks;
-
-
